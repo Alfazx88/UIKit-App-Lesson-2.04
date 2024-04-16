261f73464e67c1ed1d4267631a4a1a44f15a2f28
Alfazx88 <alfazx@yandex.ru>
2024-04-16 23:34:43 +0200

    Initial Commit

---
+++ UIKit App  Lesson 2.04/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UIKit App  Lesson 2.04
+//
+//  Created by Giovanni on 13.04.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
