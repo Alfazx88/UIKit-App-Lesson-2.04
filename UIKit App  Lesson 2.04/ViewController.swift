9db4d8587bd29761a34a0d8fbc0eca33f86e5cdc
Alfazx88 <alfazx@yandex.ru>
2024-04-18 14:05:52 +0200

    Color change logic completed

--- UIKit App  Lesson 2.04/ViewController.swift
+++ UIKit App  Lesson 2.04/ViewController.swift
@@ -7,13 +7,64 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+final class ViewController: UIViewController {
+    @IBOutlet var colorView: UIView!
+    
+    @IBOutlet var redNumberLabel: UILabel!
+    @IBOutlet var greenNumberLabel: UILabel!
+    @IBOutlet var blueNumberLabel: UILabel!
+    
+    @IBOutlet var redSlider: UISlider!
+    @IBOutlet var greenSlider: UISlider!
+    @IBOutlet var blueSlider: UISlider!
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        colorView.layer.cornerRadius = 20
+        
+        redSlider.tintColor = .red
+        greenSlider.tintColor = .green
+        blueSlider.tintColor = .blue
+        
+        redSlider.value = 0
+        greenSlider.value = 0.5
+        blueSlider.value = 1
+    }
+   
+    @IBAction func redSliderAction() {
+        redNumberLabel.text = String(format: "%.2f", redSlider.value)
+        setColor()
+    }
+    
+    @IBAction func greenSliderAction() {
+        greenNumberLabel.text = String(format: "%.2f", greenSlider.value)
+        setColor()
+    }
+    
+    @IBAction func blueSliderAction() {
+        blueNumberLabel.text = String(format: "%.2f", blueSlider.value)
+        setColor()
+    }
+    
+    private func setColor() {
+        colorView.backgroundColor = UIColor(
+            red: CGFloat(redSlider.value),
+            green: CGFloat(greenSlider.value),
+            blue: CGFloat(blueSlider.value),
+            alpha: 1)
     }
 
+    
+    
+    
 
+    
+    
+    
 }
+
+
+
 
